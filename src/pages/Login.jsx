9c8d499dbc773c169a03c7ec716c7aade90a368e
shaikhsahill <shaikhsahir3040@gmail.com>
2025-09-30 04:22:31 +0530

    dev changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -73,7 +73,7 @@ const Login = () => {
     const handleLogin = (e) => {
         e.preventDefault();
         
-        navigate('/ladingpage');
+        navigate('/dashboard');
     };
 
     return (
